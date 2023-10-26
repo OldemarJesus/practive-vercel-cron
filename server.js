9a99024e58c59c3f339a6dae46deadd98b574188
Oldemar <oldemego@gmail.com>
2023-10-26 21:21:16 +0100

    fix env port

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const app = require('./src/app')
+const dotenv = require('dotenv')
 
 /**
  * Default config
  */
-const CONFIG = {
-    port: 3000
-}
+dotenv.config()
+const PORT = process.env.PORT
 
 /**
  * Start server
  */
-app.listen(CONFIG.port, () => {
-    console.log(`Server listening on http://127.0.0.1:${CONFIG.port}`)
+app.listen(PORT, () => {
+    console.log(`Server listening on http://127.0.0.1:${PORT}`)
 })
